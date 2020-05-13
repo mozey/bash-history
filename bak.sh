1568024fb2a5988f36f817c340c393914ab7690c
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2020-05-13 11:34:48 +0200

    Minor changes

--- bak.sh
+++ bak.sh
@@ -5,18 +5,19 @@
 
 KEEP=200
 BASH_HIST=~/.bash_history
-BACKUP=$BASH_HIST.$(date +%y%m)
+BACKUP=${BASH_HIST}.$(date +%y%m)
 
-if [ -s "$BASH_HIST" -a "$BASH_HIST" -nt "$BACKUP" ]; then
-  # history file is newer then backup
-  if [[ -f $BACKUP ]]; then
-    # there is already a backup
-    cp -f $BASH_HIST $BACKUP
-  else
-    # create new backup, leave last few commands and reinitialize
-    mv -f $BASH_HIST $BACKUP
-    tail -n$KEEP $BACKUP > $BASH_HIST
-    history -r
-  fi
+if [[ -s "${BASH_HIST}" && "${BASH_HIST}" -nt "$BACKUP" ]]; then
+    # History file is newer then backup
+    if [[ -f ${BACKUP} ]]; then
+        # Backup file exists, update it
+        cp -f ${BASH_HIST} ${BACKUP}
+    else
+        # Create new backup,
+        # leave last few commands and reinitialize
+        mv -f ${BASH_HIST} ${BACKUP}
+        tail -n${KEEP} ${BACKUP} > ${BASH_HIST}
+        history -r
+    fi
 fi
 
